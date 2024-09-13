e91ef40cfc7498856b39fed7cc1d56b8b4899852
Jonathan-Frey <jf223rf@student.lnu.se>
2024-09-13 16:41:11 +0200

    test movement and events

--- src/GameObjects/Cat.ts
+++ src/GameObjects/Cat.ts
@@ -4,19 +4,87 @@ export default class Cat extends GameObject {
   #name: string;
   #color: string;
   #totalTime: number = 0;
+  #x;
+  #y;
+  #left: boolean = false;
+  #up: boolean = false;
+  #right: boolean = false;
+  #down: boolean = false;
 
-  constructor(name: string, color: string) {
+  constructor(x: number, y: number, name: string, color: string) {
     super();
     this.#name = name;
     this.#color = color;
+    this.#x = x;
+    this.#y = y;
+    window.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "ArrowLeft":
+          this.#left = true;
+          break;
+        case "ArrowUp":
+          this.#up = true;
+          break;
+        case "ArrowRight":
+          this.#right = true;
+          break;
+        case "ArrowDown":
+          this.#down = true;
+          break;
+        case " ":
+          console.log("meow");
+          break;
+        default:
+          break;
+      }
+    });
+
+    window.addEventListener("keyup", (e) => {
+      switch (e.key) {
+        case "ArrowLeft":
+          this.#left = false;
+          break;
+        case "ArrowUp":
+          this.#up = false;
+          break;
+        case "ArrowRight":
+          this.#right = false;
+          break;
+        case "ArrowDown":
+          this.#down = false;
+          break;
+      }
+    });
   }
 
   protected override process(delta: number) {
-    this.#totalTime += delta;
-    if (Math.floor(this.#totalTime) % 2 === 0) {
-      console.log(
-        `Hello! I am a cat named ${this.#name} and am the color ${this.#color}`
-      );
+    if (this.#left && this.#up) {
+      this.#x -= 20 / Math.sqrt(2);
+      this.#y -= 20 / Math.sqrt(2);
+    } else if (this.#left && this.#down) {
+      this.#x -= 20 / Math.sqrt(2);
+      this.#y += 20 / Math.sqrt(2);
+    } else if (this.#right && this.#up) {
+      this.#x += 20 / Math.sqrt(2);
+      this.#y -= 20 / Math.sqrt(2);
+    } else if (this.#right && this.#down) {
+      this.#x += 20 / Math.sqrt(2);
+      this.#y += 20 / Math.sqrt(2);
+    } else if (this.#left && this.#right) {
+    } else if (this.#up && this.#down) {
+    } else if (this.#left) {
+      this.#x -= 20;
+    } else if (this.#up) {
+      this.#y -= 20;
+    } else if (this.#right) {
+      this.#x += 20;
+    } else if (this.#down) {
+      this.#y += 20;
     }
+  }
+
+  protected override render(delta: number, ctx: CanvasRenderingContext2D) {
+    ctx.fillStyle = this.#color;
+    ctx.fillRect(this.#x, this.#y, 50, 50);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,14 @@ import GameEngine from "jf-canvas-game-engine";
 const canvas = document.querySelector<HTMLCanvasElement>(
   "canvas"
 ) as HTMLCanvasElement;
+canvas.addEventListener("dblclick", () => {
+  canvas.requestFullscreen();
+});
 
 const gameEngine = new GameEngine(canvas);
-const cat = new Cat("Dino", "orange");
+const cat = new Cat(50, 50, "Dino", "orange");
+cat.addEventListener("click", () => {
+  console.log("meow");
+});
 gameEngine.addGameObject(cat);
 gameEngine.start();
