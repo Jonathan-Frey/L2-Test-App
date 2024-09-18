cd36406dcde60606dc395469c90420e3e7ee1aef
Jonathan-Frey <jf223rf@student.lnu.se>
2024-09-18 15:57:23 +0200

    test fixed game object

--- src/scenes/Level1.ts
+++ src/scenes/Level1.ts
@@ -1,4 +1,9 @@
-import { GameObject, Rectangle, Vector2D } from "jf-canvas-game-engine";
+import {
+  FixedGameObject,
+  GameObject,
+  Rectangle,
+  Vector2D,
+} from "jf-canvas-game-engine";
 import Cat from "../gameObjects/Cat";
 
 export class Level1 extends GameObject {
@@ -6,6 +11,7 @@ export class Level1 extends GameObject {
     super();
     this.addChild(new Rectangle(1000, 1000, "red"));
     this.addChild(new Cat("Dino", "orange", new Vector2D(100, 100)));
+    this.addChild(new FixedGameObject(new Vector2D(0, 0)));
   }
 
   render(ctx: CanvasRenderingContext2D) {
